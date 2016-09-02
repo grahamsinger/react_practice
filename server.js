a6fb56bcbb9ac1929fd272e4bcbe170c696fea06
Graham Singer <grahamtsinger@gmail.com>
2016-09-02 10:51:47 -0700

    adding a few more components

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 express()
     .set('view engine', 'ejs')
     .use(express.static('./public'))
-    .use(require('accounts'))
+    .use(require('./accounts'))
     .get('*', function(req, res) {
         res.render('index', {
             user: JSON.stringify(req.session.user || null)
---
+++ src/components/Page.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import * as API from '../api';
+
+export default class Page extends React.Component {
+    state = {
+        page : {}
+    }
+
+    componentDidMount() {
+        //params is in the route as ':id'
+        API.pages.child(this.props.params.id).on('value', this.updateContent);
+    }
+
+    //Funky react.js stuff
+    componentWillRecieveProps(nextProps) {
+        API.pages.child(this.props.params.id).off('value', this.updateContent);
+        API.pages.child(nextProps.params.id).on('value', this.updateContent);
+
+    }
+
+    updateContent = (snapshot) => {
+        let json = snapshot.exportVal();
+        this.setState({
+            page : json,
+            sections: json.sections
+        });
+    }
+
+    render(){
+        let sections = [];
+
+        //data is loaded
+        if(this.state.page.title) {
+            //render sections
+
+            if(this.props.user){
+                sections.push(<p key='addSection'>
+                    <button onClick={this.addSection}> Add Section </button>
+                </p>);
+            }
+        }
+
+        return <article>
+            <h1> {this.state.page.title || 'Loading...'} </h1>
+            {sections}
+        </article>;
+
+    }
+
+    addSection = evt => {
+        let id;
+
+        if(!this.state.sections) {
+            id= 1;
+            this.state.sections = {};
+        } else {
+            id = Math.max(...Object.keys(this.state.sections)) + 1;
+        }
+
+        this.state.sections[id] = {
+            editor: this.props.user.username
+        };
+
+        this.setState({
+            sections: this.state.sections
+        });
+    }
+
+}
